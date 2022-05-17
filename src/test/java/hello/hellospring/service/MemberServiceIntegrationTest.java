703e74f31d348e615871447f9bc0973cdd4a3450
saemyunglim <saemyung2000@daum.net>
2022-05-17 22:14:35 +0900

    @SpringBootTest와 @Transactional을 이용한 스프링 통합 테스트完

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,29 +1,26 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class MemberServiceTest {
+@SpringBootTest//스프링 컨테이너와 테스트가 모두 동작하도록 하는 어노테이션
+@Transactional //@Transactional이 붙은 임의의 메서드는 해당 메서드의 명령이 모두 성공하지 않으면 예외처리시켜 롤백시켜버림(모두 성공할 경우에만 커밋함). 단, 테스트 코드에서는 해당 메서드의 명령어가 모두 실행되고난 후에 롤백되어 DB에 들어갔던 데이터가 모두 초기화됨.
+public class MemberServiceIntegrationTest {
 
-    MemoryMemberRepository memoryMemberRepository;
-    MemberService memberService;
 
-    @BeforeEach
-    public void beforeEach() {
-        memoryMemberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memoryMemberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memoryMemberRepository.clearStore();
-    }
+    @Autowired MemberRepository memberRepository; //테스트 코드는 다른 곳에서 사용하는 경우가 없기때문에 생성자로 주입시킬 필요가 없음
+                                                  //@Autowired를 통해 지정한 타입에 해당하는 객체를 가져옴
+    @Autowired MemberService memberService;
 
     @Test
     void 회원가입() {
@@ -61,13 +58,5 @@ class MemberServiceTest {
         //then
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
     }
 }
